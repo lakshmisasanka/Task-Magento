cc3c52ad9449706d27addf75fc39439ea53d5c23
lakshmisasanka <lakshmi.sasanka@ziffity.com>
2022-02-01 16:42:23 +0530

    Added MiniCart Cutomization

---
+++ CustomReorder/Reorder/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    "map": {
+        "*": {
+            'Magento_Checkout/template/minicart/content.html': 'CustomReorder_Reorder/template/minicart/content.html'
+        }
+    }
+};
